12eb4571e8967eccfd9591b6dd2471ba249b3d72
Samuel Hanlan <samhanlan@gmail.com>
2019-11-03 19:50:29 +0100

    ss - add click listener for scroll-here which auto-scrolls to listen section

--- src/page-singer-songwriter/singer-songwriter.js
+++ src/page-singer-songwriter/singer-songwriter.js
@@ -2,5 +2,14 @@ import './singer-songwriter.scss'
 
 window.onload = function() {
     document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
-    document.getElementById('soundcloud-widget').innerHTML = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/181077470&color=%23dfe3ba&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=true"></iframe>';
+    document.getElementById('soundcloud-widget').innerHTML = '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/181077470&color=%23dfe3ba&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=true"></iframe>';
+    const scrollHereEl = document.querySelector('.scroll-here');
+    const contentViewerEl = document.querySelector('.content-listen');
+
+    scrollHereEl.addEventListener('click', () => {
+        contentViewerEl.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    });
 }
