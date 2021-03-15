e6b8a91881aaeaeedfe527e1597004ac4efa230f
samhanlan <samhanlan@gmail.com>
2021-03-15 22:47:44 +0100

    prettify webpack.prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin")
   .default;
 const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
 
-const getHtmlPluginConfig = name => ({
+const getHtmlPluginConfig = (name) => ({
   template: `./src/page-${name}/${name}.html`,
   inject: true,
   inlineSource: ".js$",
@@ -17,37 +17,37 @@ const getHtmlPluginConfig = name => ({
   minify: {
     removeComments: true,
     removeAttributeQuotes: true,
-    collapseWhitespace: true
-  }
+    collapseWhitespace: true,
+  },
 });
 
 module.exports = merge(common, {
   mode: "production",
   output: {
-    publicPath: "http://samhanlan.com/"
+    publicPath: "http://samhanlan.com/",
   },
   plugins: [
     new MiniCssExtractPlugin({
       moduleFilename: ({ name }) =>
         name === "index"
           ? "index.[contentHash].css"
-          : `${name}/${name}.[contentHash].css`
+          : `${name}/${name}.[contentHash].css`,
     }),
     new HtmlWebpackPlugin(getHtmlPluginConfig("index")),
     new HtmlWebpackPlugin(getHtmlPluginConfig("singer-songwriter")),
     new HtmlWebpackPlugin(getHtmlPluginConfig("freedom-and-such")),
     new HtmlWebpackPlugin(getHtmlPluginConfig("audio-engineer")),
     new HTMLInlineCSSWebpackPlugin(),
-    new HtmlWebpackInlineSourcePlugin()
+    new HtmlWebpackInlineSourcePlugin(),
   ],
   optimization: {
-    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()]
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
@@ -55,11 +55,11 @@ module.exports = merge(common, {
           {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env"]
-            }
-          }
-        ]
-      }
-    ]
-  }
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
 });
